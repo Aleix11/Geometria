d528fa60bf7ac89777b44354af3d74103b32f906
aleix <aleix11fcb@gmail.com>
2017-02-28 19:37:18 +0100

    project files

---
+++ src/main/Circulo.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * Created by aleix on 28/2/2017.
+ */
+public class Circulo {
+
+    private double r1;
+
+    public Circulo(double c1, double c2)
+    {
+        this.r1 = r1;
+    }
+
+    public double area()
+    {
+        return (this.r1*this.r1*3.14);
+    }
+}
