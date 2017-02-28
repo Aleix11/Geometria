2c336dba02914b14fa5ec39363e37ef156d585a1
aleix <aleix11fcb@gmail.com>
2017-02-28 19:28:26 +0100

    ...

--- src/main/App.java
+++ src/main/App.java
@@ -1,12 +1,14 @@
 package main;
 
-import javax.print.DocFlavor;
-
-
-
+/**
+ * Created by aleix on 28/2/2017.
+ */
 public class App {
 
     public static void main (String[] args)
     {
+        Controlador lasfiguras = new Controlador();
+
+        lasfiguras.add();
     }
 }
