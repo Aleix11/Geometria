4a8a5a9627025b149d32531cc9eaed76fd33d76b
Soy programador <mikel.ga22@gmail.com>
2017-03-01 13:23:20 +0100

    Comit

--- src/main/App.java
+++ src/main/App.java
@@ -8,23 +8,23 @@ public class App {
 
     public static void main (String[] args)
     {
-        Controlador figuraas = new Controlador();
+        Controlador figuras = new Controlador();
 
         //añadimos figuras
 
-        figuraas.añadirFigura(new Circulo(2));
-        figuraas.añadirFigura(new Cuadrado(3));
-        figuraas.añadirFigura(new Rectangulo(4, 5));
-        figuraas.añadirFigura(new Triangulo(4, 2));
+        figuras.añadirFigura(new Circulo(2));
+        figuras.añadirFigura(new Cuadrado(3));
+        figuras.añadirFigura(new Rectangulo(4, 5));
+        figuras.añadirFigura(new Triangulo(4, 2));
 
         //Sumamos el total de las àreas
-        System.out.println("Suma total de las areas: " + figuraas.suma() + "\n");
+        System.out.println("Suma total de las areas: " + figuras.suma() + "\n");
 
         //Printamos la lista
         int i = 0;
-        for(i=0; i <figuraas.getFiguras().size(); i++)
+        for(i=0; i <figuras.getFiguras().size(); i++)
         {
-            System.out.println("Nombre figura: " + figuraas.getFiguras().get(i).getClass().getSimpleName() + ", Y su àrea es: " + figuraas.getFiguras().get(i).area()+"\n");
+            System.out.println("Nombre figura: " + figuras.getFiguras().get(i).getClass().getSimpleName() + ", Y su àrea es: " + figuras.getFiguras().get(i).area()+"\n");
         }
     }
 }
--- src/main/Circulo.java
+++ src/main/Circulo.java
@@ -1,22 +1,22 @@
 package main;
 
-public class Circulo {
-    double c1;
+public class Circulo implements Figura {
+    double r;
 
     public Circulo(double c1) {
-        this.c1 = c1;
+        this.r = c1;
 
     }
 
-    public double getC1() {
-        return c1;
+    public double getR() {
+        return r;
     }
 
-    public void setC1(double c1) {
-        this.c1 = c1;
+    public void setR(double r) {
+        this.r = r;
     }
 
     public double area(){
-        return Math.PI * Math.pow(c1,2);
+        return Math.PI * Math.pow(r,2);
     }
 }
