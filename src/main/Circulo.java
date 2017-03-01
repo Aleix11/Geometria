670e584cb2f7d518be12b8979f5505ee87bbff19
guillem74 <guillemeetac@gmail.com>
2017-03-01 09:45:55 +0100

    Circulo

--- src/main/Circulo.java
+++ src/main/Circulo.java
@@ -1,4 +1,22 @@
 package main;
 
 public class Circulo {
+    double c1, c2;
+
+    public Circulo(double c1) {
+        this.c1 = c1;
+
+    }
+
+    public double getC1() {
+        return c1;
+    }
+
+    public void setC1(double c1) {
+        this.c1 = c1;
+    }
+
+    public double area(){
+        return Math.PI * Math.pow(c1,2);
+    }
 }
